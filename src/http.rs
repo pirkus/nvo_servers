153064e151c6215110a24d716662d36f6ae7badf
pirkus <filip@lovich.us>
2024-08-02 19:44:10 -0400

    Remove mio

--- src/http.rs
+++ src/http.rs
@@ -8,6 +8,5 @@ pub mod blocking_http_server;
 pub mod conn_state;
 pub mod handler;
 pub mod http_status;
-pub mod mio_async_http_server;
 mod request;
 pub mod response;
--- src/http/handler.rs
+++ src/http/handler.rs
@@ -2,13 +2,9 @@ use crate::http::conn_state::ConnState;
 use crate::http::request::Request;
 use crate::http::response::Response;
 use log::{debug, error};
-use mio::event::Event;
-use mio::net::TcpStream;
-use mio::{Interest, Registry};
 use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
 use std::io;
-use std::io::ErrorKind::WouldBlock;
 use std::io::{Read, Write};
 
 #[derive(Clone, Debug)]
@@ -138,93 +134,6 @@ impl Handler {
                 Some((connection, ConnState::Flush))
             }
         }
-    }
-
-    pub fn handle_async_mio(
-        registry: &Registry,
-        connection: &mut TcpStream,
-        event: &Event,
-        conn_state: &ConnState,
-        endpoints: &HashMap<String, Handler>,
-    ) -> Result<ConnState, String> {
-        let mut conn_state = conn_state.clone();
-        if let ConnState::Read(mut req, mut read) = conn_state {
-            while read < 4 || &req[read - 4..read] != b"\r\n\r\n" {
-                let mut buf = [0u8; 1024];
-                match connection.read(&mut buf) {
-                    Ok(0) => {
-                        debug!("client hung up");
-                        return Ok(ConnState::Flush);
-                    }
-                    Ok(n) => {
-                        req.extend(buf.iter().clone());
-                        read += n;
-                    }
-                    Err(e) if e.kind() == WouldBlock => return Ok(ConnState::Read(req, read)),
-                    Err(e) => panic!("{}", e),
-                }
-            }
-
-            let raw_req = String::from_utf8_lossy(&req[..read]);
-            let request: Vec<&str> = raw_req.split('\n').collect();
-
-            let first_line: Vec<&str> = request[0].split(' ').collect();
-            let method = first_line[0];
-            let path = first_line[1];
-            let _protocol = first_line[2];
-            let _headers = &request[1..];
-
-            let endpoint_key = Handler::gen_key_from_str(path, method);
-            let endpoint = endpoints.get(&endpoint_key);
-
-            debug!("Request payload: {:?}", request);
-
-            match endpoint {
-                None => {
-                    debug!(
-                        "No handler registered for path: '{path}' and method: {method} not found."
-                    );
-                    conn_state =
-                        ConnState::Write(Request::create(path, Handler::not_found(method)), 0)
-                }
-                Some(endpoint) => {
-                    conn_state = ConnState::Write(Request::create(path, endpoint.clone()), 0)
-                }
-            }
-        }
-
-        if let ConnState::Write(req, mut written) = conn_state {
-            let res = (req.endpoint.handler_func)(&req).unwrap(); // TODO: catch panics
-            let status_line = res.get_status_line();
-            let contents = res.get_body();
-            let length = contents.len();
-            let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
-            let response_len = response.len();
-            while written != response_len {
-                match connection.write(&response.as_bytes()[written..]) {
-                    Ok(0) => {
-                        debug!("client hung up");
-                        return Ok(ConnState::Flush);
-                    }
-                    Ok(n) => {
-                        written += n;
-                        registry
-                            .reregister(connection, event.token(), Interest::READABLE)
-                            .map_err(|e| e.to_string())?;
-                    }
-                    Err(ref err) if err.kind() == WouldBlock => {}
-                    // Is this needed?
-                    // Err(ref err) if err.kind() == Interrupted => {
-                    //     return handle_connection_event(registry, connection, event, conn_state)
-                    // }
-                    Err(err) => return Err(err.to_string()),
-                }
-            }
-
-            return Ok(ConnState::Flush);
-        }
-
-        Ok(conn_state.clone())
     }
 
     pub fn new(
