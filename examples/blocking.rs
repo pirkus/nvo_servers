e811ba0e53f301b70a4d0d3d29db41ee5b3c6591
pirkus <filip@lovich.us>
2024-10-25 08:02:15 -0400

    comment out tests for blocking server

--- examples/blocking.rs
+++ examples/blocking.rs
@@ -5,8 +5,9 @@ use nvo_servers::http::handler::Handler;
 use nvo_servers::http::response::Response;
 use serde_json::json;
 use std::collections::HashSet;
-
+*/
 fn main() {
+    /*
     let status_endpoint = Handler::new("/status", "GET", |_| {
         Ok(Response::create(200, json!({"status": "ok"}).to_string()))
     });
@@ -14,5 +15,5 @@ fn main() {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     HttpServer::create_port(8090, HashSet::from([status_endpoint])).start_blocking()
+    */
 }
-*/
